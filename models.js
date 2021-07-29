da0b5d7365b4e608139cc730bc09d986bafa5b1d
obz <songbaizhiv@gmail.com>
2021-07-29 10:52:24 +0800

    删除多余接口

--- models.js
+++ models.js
@@ -7,77 +7,6 @@ mongoose.connect('mongodb://localhost:27017/game-data', {
 })
 
 
-const UserSchema =  new mongoose.Schema({
-  username: {type: String, unique: true},
-  password: {
-    type: String, 
-    set(val) {
-      return require('bcryptjs').hashSync(val, 10)
-    }
-  }
-})
-const User = mongoose.model('User', UserSchema)
-
-
-const ProfileSchema =  new mongoose.Schema({
-  imageUrl: { type: String },
-  name: {type: String },
-  bname: {type: String},
-  nav: { type: Array },
-  description: { type: String },
-  footer: { type: String },
-  created_at: { type: String, unique: true },
-})
-const Profile = mongoose.model('Profile', ProfileSchema)
-
-
-const ArticleSchema = new mongoose.Schema({
-  title: { type: String },
-  description: {type: String},
-  img: { type: String },
-  body: { type: String },
-  created_at: { type: String },
-  view: { type: Number },
-  zan: { type: Array },
-  url: { type: String , unique: true }
-})
-
-const Article = mongoose.model('Article', ArticleSchema)
-
-
-//总留言
-const Message = mongoose.model('Message', new mongoose.Schema({
-	username: { type: String },
-	message: { type: String },
-	email: { type: String },
-	url: { type: String },
-	ip: { type: String },
-	city: { type: String},
-	created_at: { type: String },
-  show: { type: Boolean },
-  avatar: { type: String }
-}))
-
-//评论
-const Reply = mongoose.model('Reply', new mongoose.Schema({
-  os_info: { type: String },
-  browser_info : { type: String },
-  city: { type: String },
-  content: { type: String },
-  ip: { type: String },
-  name: { type: String},
-  created_at: { type: String },
-  avatar: { type: String },
-  site: { type: String },
-  email: { type: String },
-  article_id : { type: String, unique: false }
-}))
-
-const Weibo = mongoose.model('Weibo', new mongoose.Schema({
-  oid: { type: String },
-  mobile : { type: String }
-}))
-
 const Gameinfo = mongoose.model('Gameinfo', new mongoose.Schema({
   wallet_address: {type: String, required: true},
   invite_id: { type: String, immutable: true }, // 邀请人ID
@@ -94,7 +23,6 @@ const Gameinfo = mongoose.model('Gameinfo', new mongoose.Schema({
   give: { type: Boolean, default: false }, // 是否发放奖励
 }))
 
-// 清空user表
-// User.db.dropCollection('users')
+// 清空表
 // Gameinfo.db.dropCollection('gameinfos')
-module.exports = { User, Profile, Article, Message, Reply, Weibo, Gameinfo }
+module.exports = { Gameinfo }
